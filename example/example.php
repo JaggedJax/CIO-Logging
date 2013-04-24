d87a9706d00dc7984e2323ce7190feda0fae98ce
William W <Anxiety35@gmail.com>
2013-04-24 12:21:22 -0700

    Example now shows Log IDs

--- example/example.php
+++ example/example.php
@@ -3,18 +3,34 @@
 # Should log to the same directory as this file
 require dirname(__FILE__) . '/../src/CIOLogger.php';
 
-$log   = CIOLogger::instance(dirname(__FILE__), CIOLogger::DEBUG);
+$log   = CIOLogger::instance(dirname(__FILE__), CIOLogger::DEBUG, 'Test');
 $args1 = array('a' => array('b' => 'c'), 'd');
 $args2 = NULL;
 
-$log->logInfo('Info Test');
-$log->logNotice('Notice Test');
-$log->logWarn('Warn Test');
-$log->logError('Error Test');
-$log->logFatal('Fatal Test');
-$log->logAlert('Alert Test');
-$log->logCrit('Crit test');
-$log->logEmerg('Emerg Test');
+if(($id == $log->logInfo('Info Test'))){
+    echo "Info Log ID: $id\n";
+}
+if(($id == $log->logNotice('Notice Test'))){
+    echo "Notice Log ID: $id\n";
+}
+if(($id == $log->logWarn('Warn Test'))){
+    echo "Warn Log ID: $id\n";
+}
+if(($id == $log->logError('Error Test'))){
+    echo "Error Log ID: $id\n";
+}
+if(($id == $log->logFatal('Fatal Test'))){
+    echo "Fatal Log ID: $id\n";
+}
+if(($id == $log->logAlert('Alert Test'))){
+    echo "Alert Log ID: $id\n";
+}
+if(($id == $log->logCrit('Crit test'))){
+    echo "Crit Log ID: $id\n";
+}
+if(($id == $log->logEmerg('Emerg Test'))){
+    echo "Emerg Log ID: $id\n";
+}
 
 $log->logInfo('Testing passing an array or object', $args1);
 $log->logWarn('Testing passing a NULL value', $args2);
